7836f561844be7bdee94a0564844b7285c3d81f4
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-01-14 13:58:45 +0100

    Add functionality to load JS pages in index.js
    Used for loading the navbar js

--- index.js
+++ index.js
@@ -16,10 +16,20 @@ function loadCSS(filename) {
   document.head.append(link)
 }
 
+function loadJS(filename) {
+  let link = document.createElement("script")
+  link.src = filename
+  link.type = "text/javascript"
+  console.log(link)
+
+  document.body.append(link)
+}
+
 async function addNavbar() {
-  const navbar = await getHTML(`components/navbar/navbar.html`);
+  const navbar = await getHTML("components/navbar/navbar.html");
   document.body.prepend(navbar)
-  loadCSS(`components/navbar/navbar.css`);
+  loadCSS("components/navbar/navbar.css");
+  loadJS("components/navbar/navbar.js");
 }
 
 addNavbar()
